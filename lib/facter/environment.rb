3b8551fef27adec93619367cafba764e00bf2e52
Paolo Cadiz <pcadiz@cambridge.org>
2017-05-05 17:16:51 +0800

    updated environment fact

--- lib/facter/environment.rb
+++ lib/facter/environment.rb
@@ -1,10 +1,15 @@
-environments = [/(dev)/i, /(uat)/i, /(test-deploy)/i, /(testdeploy)/i, /(staging)/i]
-
-environments_regex = Regexp.union(environments)
-if Facter.value(:hostname).match(environments_regex)
-    Facter.add('puppet_env') do
-        setcode do
-            $1
+if Facter.value(:hostname).match(/(dev|uat|test-deploy|testdeploy|staging)/i)
+    if ($1 == 'test-deploy') || ($1 == 'testdeploy')
+        Facter.add('puppet_env') do
+            setcode do
+                'testdeploy'
+            end
+        end
+    else
+        Facter.add('puppet_env') do
+            setcode do
+                $1
+            end
         end
     end
 else

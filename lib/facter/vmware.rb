1f04fdf3d4e13412e73399b964d723b00fc9c31f
Paolo Cadiz <pcadiz@cambridge.org>
2017-05-05 15:49:10 +0800

    added puppet_env fact

--- lib/facter/vmware.rb
+++ lib/facter/vmware.rb
@@ -2,12 +2,36 @@ roles = [
     /(cjo)(?:[0-9]|[-])/i,
     /(stahl)(?:[0-9]|[-])/i,
 ]
+environments = [ '/(dev)/', '/(uat)/', '/(test-deploy)/', '/(testdeploy)/', '/(staging)/' ]
 
 roles_regex = Regexp.union(roles)
 if Facter.value(:hostname).match(roles_regex)
     Facter.add('puppet_role') do
         setcode do
             $1
+        end
+    end
+end
+
+environments_regex = Regexp.union(environments)
+if Facter.value(:hostname).match(environments_regex)
+    if ($1 == 'test-deploy') || ($1 == 'testdeploy')
+        Facter.add('puppet_env') do
+            setcode do
+                'testdeploy'
+            end
+        end
+    else
+        Facter.add('puppet_env') do
+            setcode do
+                $1
+            end
+        end
+    end
+else
+    Facter.add('puppet_env') do
+        setcode do
+            'live'
         end
     end
 end

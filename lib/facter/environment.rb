46744381e6e6b1bf6547b221c38999d9b1a58651
Paolo Cadiz <pcadiz@cambridge.org>
2017-05-05 16:14:58 +0800

    separated facts, kept basic

---
+++ lib/facter/environment.rb
@@ -0,0 +1,16 @@
+environments = [ 
+	/(dev)/i,
+	/(uat)/i,
+	/(test-deploy)/i,
+	/(testdeploy)/i,
+	/(staging)/i
+]
+
+environments_regex = Regexp.union(environments)
+if Facter.value(:hostname).match(environments_regex)
+    Facter.add('puppet_env') do
+        setcode do
+            $1
+        end
+    end
+end

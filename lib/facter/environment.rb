133eb17ebd9e79b1938dd0e5e065ba43f7f7f39a
Paolo Cadiz <pcadiz@cambridge.org>
2017-05-05 16:08:13 +0800

    separated facts

--- lib/facter/environment.rb
+++ lib/facter/environment.rb
@@ -1,17 +1,4 @@
-roles = [
-    /(cjo)(?:[0-9]|[-])/i,
-    /(stahl)(?:[0-9]|[-])/i,
-]
 environments = [ /(dev)/i, /(uat)/i, /(test-deploy)/i, /(testdeploy)/i, /(staging)/i ]
-
-roles_regex = Regexp.union(roles)
-if Facter.value(:hostname).match(roles_regex)
-    Facter.add('puppet_role') do
-        setcode do
-            $1
-        end
-    end
-end
 
 environments_regex = Regexp.union(environments)
 if Facter.value(:hostname).match(environments_regex)
---
+++ lib/facter/role.rb
@@ -0,0 +1,13 @@
+roles = [
+    /(cjo)(?:[0-9]|[-])/i,
+    /(stahl)(?:[0-9]|[-])/i,
+]
+
+roles_regex = Regexp.union(roles)
+if Facter.value(:hostname).match(roles_regex)
+    Facter.add('puppet_role') do
+        setcode do
+            $1
+        end
+    end
+end
